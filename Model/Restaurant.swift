b88d48dd3b66565b1fb0dcb9587c355d55ced4fd
Doniyor Normaxammedov <donioscode@gmail.com>
2024-11-19 13:33:57 +0500

    new feature

---
+++ Model/Restaurant.swift
@@ -0,0 +1,16 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by doniyor normuxammedov on 19/11/24.
+//
+
+
+struct Restaurant: Hashable {
+    var name: String = ""
+    var type: String = ""
+    var location: String = ""
+    var image: String = ""
+    var isFavorite: Bool = false
+    
+}
