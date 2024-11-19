ba15aba7932c0f24c19d96cf05665a2a1c7634f4
Doniyor Normaxammedov <donioscode@gmail.com>
2024-11-19 15:48:54 +0500

    Swipe-to-delete and share feature traling added

---
+++ FoodPin/Controller/RestaurantDiffableDataSource.swift
@@ -0,0 +1,12 @@
+//
+//  RestaurantDiffableDataSource.swift
+//  FoodPin
+//
+//  Created by doniyor normuxammedov on 19/11/24.
+//
+
+import UIKit
+
+class RestaurantDiffableDataSource: UITableViewDiffableDataSource {
+
+}
