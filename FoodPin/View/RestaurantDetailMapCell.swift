e9b97b85b41b6da0dbd16ef212e4da6c6ad5fc7b
Doniyor Normaxammedov <donioscode@gmail.com>
2025-01-24 23:14:02 +0500

    add new file  RestaurantDetailMapCell

---
+++ FoodPin/View/RestaurantDetailMapCell.swift
@@ -0,0 +1,23 @@
+//
+//  RestaurantDetailMapCell.swift
+//  FoodPin
+//
+//  Created by doniyor normuxammedov on 28/11/24.
+//
+
+import UIKit
+
+class RestaurantDetailMapCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
